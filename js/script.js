939e276441b0ecf8565a936a207beaba1e54ce83
Evelien Rutsaert <rutsaert@Eveliens-MBP.lan>
2020-03-23 08:40:50 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -18,20 +18,16 @@ let books = [{
 
 // Stap 1: loop door een Array, de array bevat 3 objecten.
 //Opties: .map, .foreach, for-loop
-books.forEach(book => {
+
     // Console log het 'boektitel door boekauteur'
-    console.log(`${book.title} door ${book.author}`);
-});
+
+
+
 
 // Stap 2: loop door een Array, de array bevat 3 objecten.
 
-books.forEach(book => {
     // Ga na als de alreadyRead van het object book een true teruggeeft
-    if (book.alreadyRead) {
+
         //Indien alreadyRead true: console.log 'Je las reeds het boek ... ' 
-        console.log(`Je las reeds het boek ${book.title} door ${book.author}`);
-    }else{
+
         //Indien alreadyRead false: console.log 'Je moet het boek ...  nog lezen'
-        console.log(`Je moet het boek ${book.title} door ${book.author} no lezen`);
-    }
-});
